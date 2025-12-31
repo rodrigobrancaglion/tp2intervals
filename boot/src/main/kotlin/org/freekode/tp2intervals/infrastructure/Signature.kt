627e778bc1d9eb82615611754c252575fdff8dbe
RODRIGO BRANCAGLION <rodrigo.brancaglion@gmail.com>
2025-12-31 21:29:29 +0100

    Deprecated Signature.description

--- boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/Signature.kt
+++ boot/src/main/kotlin/org/freekode/tp2intervals/infrastructure/Signature.kt
@@ -2,8 +2,9 @@ package org.freekode.tp2intervals.infrastructure
 
 class Signature {
     companion object {
-        val description = """
-            Imported with tp2intervals (https://github.com/freekode/tp2intervals)
-        """.trimIndent()
+//        val description = """
+//            Imported with tp2intervals (https://github.com/freekode/tp2intervals)
+//        """.trimIndent()
+        val description = """""".trimIndent()
     }
 }
